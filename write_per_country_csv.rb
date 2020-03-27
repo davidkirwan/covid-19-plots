2546c0a557bddd9b491018e6235fb07d1c9faab9
David Kirwan <dkirwan@redhat.com>
2020-03-27 15:06:16 +0000

    Ruby script for creating data which will feed into new plots shortly

--- write_per_country_csv.rb
+++ write_per_country_csv.rb
@@ -6,6 +6,7 @@
 # Working with the COVID-19 data
 ######################################################
 require "csv"
+require "date"
 
 data = []
 # Read in the data
@@ -25,14 +26,14 @@ csv_files.each do |csv_file|
 
   # Headings
   # Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered,Latitude,Longitude
-  state = results[0].find_index("Province/State")
-  country = results[0].find_index("Country/Region")
-  last_update = results[0].find_index("Last Update")
+  state = results[0].find_index("Province/State") || results[0].find_index("Province_State")
+  country = results[0].find_index("Country/Region") || results[0].find_index("Country_Region")
+  last_update = results[0].find_index("Last Update") || results[0].find_index("Last_Update")
   confirmed = results[0].find_index("Confirmed")
   deaths = results[0].find_index("Deaths")
   recovered = results[0].find_index("Recovered")
-  latitude = results[0].find_index("Latitude")
-  longitude = results[0].find_index("Longitude")
+  latitude = results[0].find_index("Latitude") || results[0].find_index("Lat")
+  longitude = results[0].find_index("Longitude") || results[0].find_index("Long_")
 
   results[1..results.size].each do |i|
     c = i[country]
@@ -41,6 +42,7 @@ csv_files.each do |csv_file|
     end
     if countries[c].nil?
       countries[c] = {
+        "date" => File.basename(csv_file, ".*"),
         "total_confirmed" => i[confirmed].to_i,
         "total_deaths" => i[deaths].to_i,
         "total_recovered" => i[recovered].to_i,
@@ -49,6 +51,7 @@ csv_files.each do |csv_file|
         "recovery_rate" => (i[recovered].to_f / i[confirmed].to_i * 100).round(2)
       }
     else
+      countries[c]["date"] = File.basename(csv_file, ".*")
       countries[c]["total_confirmed"] += i[confirmed].to_i
       countries[c]["total_deaths"] += i[deaths].to_i
       countries[c]["total_recovered"] += i[recovered].to_i
@@ -61,24 +64,26 @@ csv_files.each do |csv_file|
   data << countries
 end
 
-puts data.size
-puts data.first
-puts data.last
-puts data.last.keys
+countries_list = data.last.keys
 
-=begin
-CSV.open("covid-19.csv", "wb") do |csv|
-  # Write the CSV report titles
-  csv << ["date", "total_confirmed", "total_deaths", "total_recovered", "total_infected", "mortality_rate", "recovery_rate"]
+countries_list.each do |c|
+  c_name = c.downcase.strip.tr(" ", "_").tr("(", "").tr(")","").tr("*","").tr("'","")
+  puts c_name
+  CSV.open("countries/covid-19_#{c_name}.csv", "wb") do |csv|
+    # Write the CSV report titles
+    csv << ["date", "total_confirmed", "total_deaths", "total_recovered", "total_infected", "mortality_rate", "recovery_rate"]
+    data.each do |d|
+      unless d[c].nil?
+        date = d[c]["date"]
+        total_confirmed = d[c]["total_confirmed"]
+        total_deaths = d[c]["total_deaths"]
+        total_recovered = d[c]["total_recovered"]
+        total_infected = d[c]["total_infected"]
+        mortality_rate = d[c]["mortality_rate"]
+        recovery_rate = d[c]["recovery_rate"]
 
-  date = File.basename(csv_file, ".*")
-  puts "Data for day: #{date}"
-  puts "Total Confirmed: #{total_confirmed}"
-  puts "Total Deaths: #{total_deaths}"
-  puts "Total Recovered: #{total_recovered}"
-  puts "Total Infected: #{total_infected}"
-  puts "Mortality Rate: #{mortality_rate}%"
-  puts "Recovery Rate: #{recovery_rate}%"
-  csv << [date, total_confirmed, total_deaths, total_recovered, total_infected, mortality_rate, recovery_rate]
+        csv << [date, total_confirmed, total_deaths, total_recovered, total_infected, mortality_rate, recovery_rate]
+      end
+    end
+  end
 end
-=end
